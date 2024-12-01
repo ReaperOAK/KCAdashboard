bb71a5b1248161db8f7bcd5e297676dee940323c
ReaperOAK <oaak78692@gmail.com>
2024-12-01 22:47:25 +0530

    testttttttt

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Footer from './components/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import ContactUs from './pages/ContactUs';
-import StudentDashboard from './pages/StudentDashboard';
-import TeacherDashboard from './pages/TeacherDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import StudentAttendance from './pages/StudentAttendance';
-import TeacherAttendance from './pages/TeacherAttendance';
-import AdminAttendance from './pages/AdminAttendance';
-import Resources from './pages/Resources';
-import GradingFeedback from './pages/GradingFeedback';
-import BatchManagement from './pages/BatchManagement';
-import AnalyticsReporting from './pages/AnalyticsReporting';
-import Settings from './pages/Settings';
-import Support from './pages/Support';
 import ResetPassword from './pages/ResetPassword';
-import ProtectedRoute from './components/ProtectedRoute';
-import useTokenValidation from './utils/useTokenValidation';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-        </Routes>
-      {/* <AppContent /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
     </Router>
   );
 };
 
+// Commented out unused code for now
 // const AppContent = () => {
 //   const [role, setRole] = useState('');
 

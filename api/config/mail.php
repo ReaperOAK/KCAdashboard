5629a1be5df468ea9acfb3a24ebc6f14fe12340e
ReaperOAK <136883103+ReaperOAK@users.noreply.github.com>
2025-02-17 16:34:09 +0000

    Build: (cdd834a) mailer

---
+++ api/config/mail.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'smtp_host' => 'smtp.hostinger.com',
+    'smtp_port' => 465,
+    'smtp_username' => 'noreply@kolkatachessacademy.in',
+    'smtp_password' => 'Admin1234*',
+    'from_email' => 'admin@kolkatachessacademy.in',
+    'from_name' => 'Kolkata Chess Academy'
+];
+?>

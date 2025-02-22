be025297750721a4f0ef818fe1d73029042911fa
ReaperOAK <136883103+ReaperOAK@users.noreply.github.com>
2025-02-22 05:50:36 +0000

    Build: (04fa0f3) check

--- api/config/cors.php
+++ api/config/cors.php
@@ -1,16 +1,5 @@
 <?php
-// Allow multiple origins
-$allowed_origins = array(
-    'http://localhost:3000',
-    'http://dashboard.kolkatachessacademy.in'
-);
-
-$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-
-if (in_array($origin, $allowed_origins)) {
-    header("Access-Control-Allow-Origin: " . $origin);
-}
-
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
 header("Access-Control-Max-Age: 3600");
@@ -19,7 +8,7 @@ header("Access-Control-Allow-Credentials: true");
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("HTTP/1.1 200 OK");
-    exit();
+    http_response_code(200);
+    exit;
 }
 ?>
